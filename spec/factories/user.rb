0c1f29d0873b51d10822502dca6fb9d66fc592ca
mjunior <mauriciojs.junior@gmail.com>
2017-10-21 14:15:59 -0200

    Created factories

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+ factory :user do
+   name         { FFaker::Lorem.word }
+   email        { FFaker::Internet.email }
+   password     'secret123'
+   password_confirmation     'secret123'
+ end
+end
