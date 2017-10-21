69691bedf13695b20d653378264cd1b9d2e535a0
mjunior <mauriciojs.junior@gmail.com>
2017-10-21 20:39:04 -0200

    Added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include CanCan::ControllerAdditions
   
+  Rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
+    
 end
